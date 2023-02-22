4d8ca9613029103984eb40083d65d7f2542f7107
jiriki79 <43390202+Jiriki79@users.noreply.github.com>
2023-02-22 13:18:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,71 +2,60 @@ require([
     "esri/config",
      "esri/Map",
      "esri/views/MapView",
-     "esri/layers/FeatureLayer"
-   ], function (esriConfig,Map, MapView,FeatureLayer) {
+     "esri/layers/FeatureLayer",
+     "esri/layers/GraphicsLayer",
+     "esri/Graphic",
+     "esri/geometry/geometryEngine"
+   ], function (esriConfig,Map, MapView,FeatureLayer,geometryEngine,GraphicsLayer,Graphic) {
 
      esriConfig.apiKey = "AAPK4cf070f48d9447b2af48df14234fb1c1iQX7jgpsU_O_g6eHZ7JOvND6f5_H5aiAembWWExxH_7g1-pPgwzqDxd8MJe2N5JZ";
      const map = new Map({
        basemap: "arcgis-topographic" // Basemap layer
      });
+     const bufferLayer = new GraphicsLayer();
+     const graphicsLayer = new GraphicsLayer();
 
+     
      const view = new MapView({
        map: map,
        center: [-97.305923,37.679377],
        zoom: 10, // scale: 72223.819286
        container: "viewDiv"
      });
-     const popupTrails = {
-        title: "Trail Information",
-        content: [{
-         type: "media",
-          mediaInfos: [{
-            type: "column-chart",
-            caption: "",
-            value: {
-              fields: [ "place_name","ObjectID" ],
-              normalizeField: null,
-              tooltipField: "test"
-              }
-            }]
-        }]
-      };
-      const popupOpenspaces = {
-        "title": "test",
-        "content": [{
-          "type": "fields",
-          "fieldInfos": [
-            {
-              "fieldName": "place_name",
-              "label": "location name",
-              "isEditable": true,
-              "tooltip": "",
-              "visible": true,
-              "format": null,
-              "stringFieldOption": "text-box"
-            },
-            {
-              "fieldName": "ObjectId",
-              "label": "ID",
-              "isEditable": true,
-              "tooltip": "",
-              "visible": true,
-              "format": null,
-              "stringFieldOption": "text-box"
-            }
-          ]
-        }]
-      };
 
-     const Content = {
+      const Content = {
         "title": "API Test",
         "content": "<b>ID:</b> {ObjectID}<br><b>Location:</b> {place_name}<br>"
       }
-     const MyLayer = new FeatureLayer({
-        url: "https://services8.arcgis.com/TrLhDuWxkcSkFAjt/arcgis/rest/services/api_test/FeatureServer/0",
-        outFields: ["place_name",'ObjectID'],
-        popupTemplate: popupOpenspaces
+      const MyLayer = new FeatureLayer({
+        url: "https://services8.arcgis.com/TrLhDuWxkcSkFAjt/arcgis/rest/services/api_test/FeatureServer/0"
       });
+      map.add(bufferLayer);
+      map.add(graphicsLayer);
       map.add(MyLayer);
-
+      var option = 0
+      document.getElementById('bufferBtn').addEventListener("click", submit)
+      function submit(){
+        const e = document.getElementById('dropdown');
+        option = e.value;
+        const sym = {
+          type: "simple-fill",
+          color: [227, 139, 79, 0.25],
+          style: "solid",
+          outline: {
+            color: [255, 255, 255, 255],
+            width: 1
+          }
+        };
+        
+        MyLayer.queryFeatures().then((results)=>{
+          results.features.map(feat => {
+            const buffer = geometryEngine.Buffer(feat.geometry, 200, "kilometers");
+            
+            bufferGraphic = new Graphic({geometry: buffer, symbol: sym});
+            // add graphic to map
+            graphicsLayer.add(bufferGraphic);
+          });
+        })
+      };
    });
